7a2084f9fdb50894ce2f76fd454d90ee7f549156
Sun flower <MLIU2@LTU.EDU>
2023-11-20 19:59:30 -0500

    create websocket server

--- index-websocket.js
+++ index-websocket.js
@@ -10,3 +10,27 @@ server.on('request', app);
 server.listen(3000, function () {
   'server started on 3000...';
 });
+
+/**** Begin websocket ******/
+const websocketServer = require('ws').Server;
+
+const wss = new websocketServer({ server });
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log('clients connected', numClients);
+
+  wss.boardcast(`current visitors ${numClients}`);
+  if (ws.readyState === ws.OPEN) {
+    ws.send('welcome to my server');
+  }
+  ws.on('close', function close() {
+    wss.boardcast(`current visitors ${numClients}`);
+    console.log('a client has disconnect');
+  });
+});
+
+wss.boardcast = function boardcast(data) {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
