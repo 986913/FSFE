b0c87b738eac222b468b4ec00f87aeb1c386ea84
Sun flower <MLIU2@LTU.EDU>
2023-11-21 21:26:22 -0500

    add database logging

--- index-websocket.js
+++ index-websocket.js
@@ -11,7 +11,14 @@ server.listen(3000, function () {
   'server started on 3000...';
 });
 
-/**** Begin websocket ******/
+process.on('SIGINT', () => {
+  wss.clients.forEach((client) => client.close());
+  server.close(() => {
+    shutDownDB();
+  });
+});
+
+/************************ Begin websocket ***********************/
 const websocketServer = require('ws').Server;
 
 const wss = new websocketServer({ server });
@@ -23,6 +30,11 @@ wss.on('connection', function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome to my server');
   }
+
+  db.run(`INSERT INTO visitors(count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on('close', function close() {
     wss.boardcast(`current visitors ${numClients}`);
     console.log('a client has disconnect');
@@ -34,3 +46,30 @@ wss.boardcast = function boardcast(data) {
     client.send(data);
   });
 };
+/************************End websocket ************************/
+
+/************************ Begin database ************************/
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutDownDB() {
+  getCounts();
+  console.log('shutting down db');
+  db.close();
+}
+/************************ End database ************************/
